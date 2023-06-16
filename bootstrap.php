8a1e66f7d4827118d2832d040f64cbdcdc5a3029
“duck7000” <ed@riethorst.net>
2023-06-16 16:53:46 +0200

    added GraphQL

--- bootstrap.php
+++ bootstrap.php
@@ -2,6 +2,7 @@
 
 require_once __DIR__ . '/src/Imdb/Config.php';
 require_once __DIR__ . '/src/Imdb/Request.php';
+require_once __DIR__ . '/src/Imdb/GraphQL.php';
 require_once __DIR__ . '/src/Imdb/Pages.php';
 require_once __DIR__ . '/src/Imdb/MdbBase.php';
 require_once __DIR__ . '/src/Imdb/Title.php';
