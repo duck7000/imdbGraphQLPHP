54fc82b065a345a328272bfc0c35ad64ef4e83b6
“duck7000” <ed@riethorst.net>
2023-08-23 22:47:33 +0200

    Added doc block to GraphQL class written by tboothman

--- src/Imdb/GraphQL.php
+++ src/Imdb/GraphQL.php
@@ -1,4 +1,12 @@
 <?php
+#############################################################################
+# GraphQL class                                           (c) tboothman     #
+# written by Tom Boothman                                                   #
+# Adjusted by Ed                                                            #
+# ------------------------------------------------------------------------- #
+# This program is free software; you can redistribute and/or modify it      #
+# under the terms of the GNU General Public License (see doc/LICENSE)       #
+#############################################################################
 
 namespace Imdb;
 
