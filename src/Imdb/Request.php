f362bb0fde5774f4b7ae5f79a6b24db6bf69142a
“duck7000” <ed@riethorst.net>
2023-12-02 14:34:26 +0100

    Request class modified to remove config and ini override

--- src/Imdb/Request.php
+++ src/Imdb/Request.php
@@ -1,10 +1,9 @@
 <?php
 
 #############################################################################
-# IMDBPHP6                             (c) Giorgos Giagas & Itzchak Rehberg #
+# IMDBPHP                              (c) Giorgos Giagas & Itzchak Rehberg #
 # written by Giorgos Giagas                                                 #
 # extended & maintained by Itzchak Rehberg <izzysoft AT qumran DOT org>     #
-# written extended & maintained by Ed                                       #
 # http://www.izzysoft.de/                                                   #
 # ------------------------------------------------------------------------- #
 # This program is free software; you can redistribute and/or modify it      #
@@ -21,52 +20,21 @@ namespace Imdb;
 class Request
 {
     private $ch;
-    private $urltoopen;
     private $page;
     private $requestHeaders = array();
     private $responseHeaders = array();
-    private $config;
 
     /**
      * No need to call this.
      * @param string $url URL to open
-     * @param Config $config The Config object to use
      */
-    public function __construct($url, Config $config)
+    public function __construct($url)
     {
-        $this->config = $config;
         $this->ch = curl_init($url);
         curl_setopt($this->ch, CURLOPT_ENCODING, "");
         curl_setopt($this->ch, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($this->ch, CURLOPT_HEADERFUNCTION, array(&$this, "callback_CURLOPT_HEADERFUNCTION"));
-
-        //use HTTP-Proxy
-        if ($config->use_proxy === true) {
-            curl_setopt($this->ch, CURLOPT_PROXY, $config->proxy_host);
-            curl_setopt($this->ch, CURLOPT_PROXYPORT, $config->proxy_port);
-
-            //Login credentials set?
-            if (!empty($config->proxy_user) && !empty($config->proxy_pw)) {
-                curl_setopt($this->ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
-                curl_setopt($this->ch, CURLOPT_PROXYUSERPWD, $config->proxy_user . ':' . $config->proxy_pw);
-            }
-        }
-
-        $this->urltoopen = $url;
-
-        $this->addHeaderLine('Referer', 'https://' . $config->imdbsite . '/');
-
-        if ($config->force_agent) {
-            curl_setopt($this->ch, CURLOPT_USERAGENT, $config->force_agent);
-        } else {
-            curl_setopt($this->ch, CURLOPT_USERAGENT, $config->default_agent);
-        }
-        if ($config->language) {
-            $this->addHeaderLine('Accept-Language', $config->language);
-        }
-        if ($config->ip_address) {
-            $this->addHeaderLine('X-Forwarded-For', $config->ip_address);
-        }
+        curl_setopt($this->ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:47.0) Gecko/20100101 Firefox/47.0');
     }
 
     public function addHeaderLine($name, $value)
@@ -78,8 +46,6 @@ class Request
      * Send a POST request
      *
      * @param string|array $content
-     * @return boolean success
-     * @throws Exception\Http
      */
     public function post($content)
     {
@@ -90,8 +56,6 @@ class Request
 
     /**
      * Send a request to the movie site
-     * @return boolean success
-     * @throws Exception\Http
      */
     public function sendRequest()
     {
@@ -115,34 +79,6 @@ class Request
     }
 
     /**
-     * Set the URL we need to parse
-     * @param string $url
-     */
-    public function setURL($url)
-    {
-        $this->urltoopen = $url;
-        curl_setopt($this->ch, CURLOPT_URL, $url);
-    }
-
-    /**
-     * Get a header value from the response
-     * @param string $header header field name
-     * @return string header value
-     */
-    public function getResponseHeader($header)
-    {
-        $headers = $this->getLastResponseHeaders();
-        foreach ($headers as $head) {
-            if (is_integer(stripos($head, $header))) {
-                $hstart = strpos($head, ": ");
-                $head = trim(substr($head, $hstart + 2, 100));
-                return $head;
-            }
-        }
-        return '';
-    }
-
-    /**
      * HTTP status code of the last response
      * @return int|null null if last request failed
      */
@@ -158,30 +94,6 @@ class Request
         }
 
         return (int)$matches[1];
-    }
-
-    /**
-     * Get the URL to redirect to if a 30* was returned
-     * @return string|null URL to redirect to if 300, otherwise null
-     */
-    public function getRedirect()
-    {
-        $status = $this->getStatus();
-        if ($status == 301 || $status == 302 || $status == 303 || $status == 307 || $status == 308) {
-            foreach ($this->getLastResponseHeaders() as $header) {
-                if (strpos(trim(strtolower($header)), 'location') !== 0) {
-                    continue;
-                }
-                $aline = explode(': ', $header);
-                $target = trim($aline[1]);
-                $urlParts = parse_url($target);
-                if (!isset($urlParts['host'])) {
-                    $initialRequestUrlParts = parse_url($this->urltoopen);
-                    $target = $initialRequestUrlParts['scheme'] . "://" . $initialRequestUrlParts['host'] . $target;
-                }
-                return $target;
-            }
-        }
     }
 
     public function getLastResponseHeaders()
