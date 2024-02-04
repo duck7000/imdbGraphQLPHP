dd4783b6ce8d2be91ae3081b16930d7b4e275139
“duck7000” <ed@riethorst.net>
2024-02-04 21:05:15 +0100

    Added localization to GraphQL

--- src/Imdb/GraphQL.php
+++ src/Imdb/GraphQL.php
@@ -18,6 +18,40 @@ namespace Imdb;
 class GraphQL
 {
 
+    /**
+     * @var boolean useLocalization set true to use localization
+     * leave this to false if you want US American English
+     */
+    protected $useLocalization = false;
+
+    /**
+     * @var string country set country code
+     * possible values: 
+     * CA (Canada)
+     * FR (France)
+     * DE (Germany)
+     * IN (Indonesia)
+     * IT (Italy)
+     * BR (Brazil)
+     * ES (Spain)
+     * MX (Mexico)
+     */
+    protected $country = "";
+
+    /**
+     * @var string language set language code
+     * possible values: 
+     * fr-CA (French Canada)
+     * fr-FR (French France)
+     * de-DE (German Germany)
+     * hi-IN (hindi)
+     * it-IT (Italian Italy)
+     * pt-BR (Portugues Brazil)
+     * es-ES (Spanisch Spain)
+     * es-MX (Spanisch Mexico)
+     */
+    protected $language = "";
+
     public function query($query, $qn = null, $variables = array())
     {
         $key = "gql.$qn." . ($variables ? json_encode($variables) : '') . md5($query) . ".json";
@@ -37,6 +71,15 @@ class GraphQL
     {
         $request = new Request('https://api.graphql.imdb.com/');
         $request->addHeaderLine("Content-Type", "application/json");
+
+        if ($this->useLocalization === true) {
+            if (!empty($this->country)) {
+                $request->addHeaderLine("X-Imdb-User-Country", $this->country);
+            }
+            if (!empty($this->language)) {
+                $request->addHeaderLine("X-Imdb-User-Language", $this->language);
+            }
+        }
 
         $payload = json_encode(
             array(
