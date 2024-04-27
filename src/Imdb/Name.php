6e6e1c504f37808b99946953cafd63329170a82a
“duck7000” <ed@riethorst.net>
2024-04-27 22:07:50 +0200

    Name class creditKnownFor added stripSpaces

--- src/Imdb/Name.php
+++ src/Imdb/Name.php
@@ -1205,7 +1205,10 @@ query KnownFor(\$id: ID!) {
   }
 }
 EOF;
-            $data = $this->graphql->query($query, "KnownFor", ["id" => "nm$this->imdbID"]);
+            // this strip spaces from $query to lower character count due hosters limit
+            $queryNode = $this->stripSpaces($query);
+            
+            $data = $this->graphql->query($queryNode, "KnownFor", ["id" => "nm$this->imdbID"]);
             if ($data != null) {
                 foreach ($data->name->knownFor->edges as $edge) {
                     $title = isset($edge->node->credit->title->titleText->text) ?
