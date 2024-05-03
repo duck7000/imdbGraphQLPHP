0be2071b909d6a094c85333b020533b50cf511c1
“duck7000” <ed@riethorst.net>
2024-05-03 21:37:57 +0200

    TitleSearchAdvanced removed strip spaces part (it is integrated in graphQl)

--- src/Imdb/TitleSearchAdvanced.php
+++ src/Imdb/TitleSearchAdvanced.php
@@ -129,10 +129,7 @@ query advancedSearch{
   }
 }
 EOF;
-        // this strip spaces from $query to lower character count due hosters limit
-        $queryStripped = implode("\n", array_map('trim', explode("\n", $query)));
-
-        $data = $this->graphql->query($queryStripped, "advancedSearch");
+        $data = $this->graphql->query($query, "advancedSearch");
         foreach ($data->advancedTitleSearch->edges as $edge) {
             $imdbId = isset($edge->node->title->id) ? str_replace('tt', '', $edge->node->title->id) : '';
             $originalTitle = isset($edge->node->title->titleText->text) ? $edge->node->title->titleText->text : '';
