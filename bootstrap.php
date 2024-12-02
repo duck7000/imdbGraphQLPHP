b63d27cd51dc1ac69d29ad2987632b44cd8ad85c
“duck7000” <ed@riethorst.net>
2024-12-02 20:45:47 +0100

    Added new class News

--- bootstrap.php
+++ bootstrap.php
@@ -10,6 +10,7 @@ require_once __DIR__ . '/src/Imdb/GraphQL.php';
 require_once __DIR__ . '/src/Imdb/MdbBase.php';
 require_once __DIR__ . '/src/Imdb/Calendar.php';
 require_once __DIR__ . '/src/Imdb/Chart.php';
+require_once __DIR__ . '/src/Imdb/News.php';
 require_once __DIR__ . '/src/Imdb/Trailers.php';
 require_once __DIR__ . '/src/Imdb/Name.php';
 require_once __DIR__ . '/src/Imdb/NameSearch.php';
