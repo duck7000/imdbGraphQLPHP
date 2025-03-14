duck7000/imdbGraphQLPHP

commit e6f064dec865665defe6b0b5e8689b79beecdff2

Fixed php 8.4 warning Title class fromSearchResults()

src/Imdb/Title.php
@@ -110,14 +110,14 @@ class Title extends MdbBase
      * @return Title
      */
     public static function fromSearchResult(
-        $id,
-        $title,
-        $orignalTitle,
-        $year,
-        $type,
-        Config $config = null,
-        LoggerInterface $logger = null,
-        CacheInterface $cache = null
+        string $id,
+        string $title,
+        string $orignalTitle,
+        int $year,
+        string $type,
+        ?Config $config = null,
+        ?LoggerInterface $logger = null,
+        ?CacheInterface $cache = null
     ) {
         $imdb = new Title($id, $config, $logger, $cache);
         $imdb->mainTitle = $title;

commit 2394df4444b5ba25201b1187574b720611c84df6

param $year is string|null in method fromSearchResult, bug introduced in commit https://github.com/duck7000/imdbGraphQLPHP/commit/e6f064dec865665defe6b0b5e8689b79beecdff2

src/Imdb/Title.php
@@ -102,7 +102,7 @@ class Title extends MdbBase
      * @param string $id imdb ID
      * @param string $title film title
      * @param string $orignalTitle Original film title
-     * @param int $year
+     * @param string|null $year
      * @param string $type
      * @param Config $config
      * @param LoggerInterface $logger OPTIONAL override default logger
@@ -113,7 +113,7 @@ class Title extends MdbBase
         string $id,
         string $title,
         string $orignalTitle,
-        int $year,
+        ?string $year,
         string $type,
         ?Config $config = null,
         ?LoggerInterface $logger = null,